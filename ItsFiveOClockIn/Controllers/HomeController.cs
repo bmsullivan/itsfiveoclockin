089255c22d13c82f28996c4fcf52d514a996c610
Brian Sullivan <bmsullivan@gmail.com>
2017-03-30 22:01:33 -0500

    Add Unalaska for Aleutian Standard Time

--- ItsFiveOClockIn/Controllers/HomeController.cs
+++ ItsFiveOClockIn/Controllers/HomeController.cs
@@ -11,45 +11,6 @@ namespace ItsFiveOClockIn.Controllers
         public ActionResult Index()
         {
             var fives = TimeZoneInfo.GetSystemTimeZones().Where(t => TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.UtcNow, t.Id).Hour == 17);
-            var offset = 16 + TimeZoneInfo.Local.BaseUtcOffset.Hours - DateTime.Now.Hour;
-            if (DateTime.Now.IsDaylightSavingTime())
-                offset += 1;
-            
-            if (offset < -11)
-            {
-                offset += 24;
-            }
-
-            if (offset > 12)
-                offset -= 24;
-
-            var places = new Dictionary<int, string>()
-            {
-                {-11, "Pago Pago"},
-                {-10, "Honolulu"},
-                {-9, "Anchorage"},
-                {-8, "Seattle"},
-                {-7, "Phoenix"},
-                {-6, "Dallas"},
-                {-5, "New York"},
-                {-4, "Santiago"},
-                {-3, "Rio de Janeiro"},
-                {-2, "Fernando de Noronha"},
-                {-1, "Ponta Delgada"},
-                {0, "Dublin"},
-                {1, "Munich"},
-                {2, "Athens"},
-                {3, "Baghdad"},
-                {4, "Moscow"},
-                {5, "Islamabad"},
-                {6, "Astana"},
-                {7, "Bangkok"},
-                {8, "Beijing"},
-                {9, "Tokyo"},
-                {10, "Sydney"},
-                {11, "Vladivostok"},
-                {12, "Auckland"}
-            };
 
             var zones = new Dictionary<string, string>
                             {
@@ -76,7 +37,8 @@ namespace ItsFiveOClockIn.Controllers
                                 {"Tokyo Standard Time", "Tokyo"},
                                 {"E. Australia Standard Time", "Sydney"},
                                 {"Russia TZ 10 Standard Time", "Vladivostok"},
-                                {"New Zealand Standard Time", "Auckland"}
+                                {"New Zealand Standard Time", "Auckland"},
+                                {"Aleutian Standard Time", "Unalaska" }
                             };
 
             string city = "";
